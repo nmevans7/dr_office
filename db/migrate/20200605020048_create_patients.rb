a3cf52381f155221ce1196e2cd9fda8f67e5ceda
natalie evans <nmevans7@gmail.com>
2020-06-04 20:06:02 -0600

    initial

---
+++ db/migrate/20200605020048_create_patients.rb
@@ -0,0 +1,12 @@
+class CreatePatients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :patients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
