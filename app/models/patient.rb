c61708ab90cc384690d69381ea35c666b26de856
Daniel McKenna <mckenna.daniel15@gmail.com>
2020-06-05 18:39:39 -0600

    added  navbar

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,3 +2,7 @@ class Patient < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :doctors, through: :appointments
 end
+
+def full_name
+'#{patient.first_name} #{patient.last_name}'
+end
